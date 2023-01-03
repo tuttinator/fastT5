fe34ff37fc9e1ddac6334bfae8d33002fc6f688e
Caleb Tutty <caleb.tutty@gmail.com>
2023-01-03 14:58:08 +0700

    feat: adjust onnxruntime dep

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     install_requires=[
         "torch>=1.7.0,!=1.8.0",  # excludes torch v1.8.0
         "onnx",
-        "onnxruntime==1.10.0",
+        "onnxruntime==1.13.1",
         "transformers>4.6.1",
         "progress>=1.5",
         "sentencepiece",
